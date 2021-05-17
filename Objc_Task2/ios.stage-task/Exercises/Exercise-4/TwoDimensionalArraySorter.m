43fe7a08e53d9a47a232de8410da548c528d01d3
brazilec22 <stratienko.artem@gmail.com>
2021-05-17 13:56:32 +0300

    ObjC Task2 - Part 4 & .gitignore

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,41 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    NSMutableArray *result = [ [NSMutableArray alloc ] init ];
+    NSMutableArray *subString = [ [NSMutableArray alloc ] init ];
+    NSMutableArray *subNumber = [ [NSMutableArray alloc ] init ];
+    if ( array == nil  ) {
+        return @[];
+    }
+    if ( ![ array.firstObject isKindOfClass: NSArray.class ] ) {
+        return @[];
+    }
+    
+    for (NSArray *arraySub in array) {
+        NSLog(@" %@", arraySub);
+        if ( [ arraySub.firstObject isKindOfClass: NSString.class ] ) {
+            [ subString addObjectsFromArray: arraySub ];
+        } else if ( [ arraySub.firstObject isKindOfClass: NSNumber.class ] ) {
+            [ subNumber addObjectsFromArray: arraySub ];
+        }
+    }
+   
+    if ( [ subString isEqualToArray: @[] ]) {
+        [ subNumber sortUsingSelector:@selector(compare:)];
+        [ result addObjectsFromArray: subNumber ];//= subNumber;
+    } else if ( [ subNumber isEqualToArray: @[] ] ) {
+        [subString sortUsingSelector:@selector(compare:)];
+        [ result addObjectsFromArray: subString ];
+    } else {
+        [ subNumber sortUsingSelector:@selector(compare:)];
+        [ result addObjectsFromArray: subNumber ];
+        [ subString sortUsingSelector:@selector(compare:)];
+        [ subString reverseObjectEnumerator];
+        [ result addObjectsFromArray: subString ];
+        
+    }
+    NSLog(@" Firs array (NSString) : %@\nSecond array (NSNumber) : %@", subString,  subNumber);
+    return result;
 }
 
 @end
