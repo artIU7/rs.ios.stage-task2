b6e3b8493ab82da429fee4eae8e5a1ce16a90ab2
brazilec22 <stratienko.artem@gmail.com>
2021-05-17 11:04:33 +0300

    ObjC Task2 - Part 1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 res = 0;
+    NSUInteger counter = 8;
+    while ( counter > 0) {
+        res = (res << 1) | (n & 1);
+        n >>= 1;
+        counter -= 1;
+    }
+    return res;
 }
